24196bc53f615e00c2b556fff87e09e9b68a2627
Paris <paris.ambush@gmail.com>
2011-03-20 18:16:09 -0700

    more accurate map

--- StemMapTest/makeMapDat.php
+++ StemMapTest/makeMapDat.php
@@ -11,22 +11,21 @@
         $parser = xml_parser_create();
 
         //Function to use at the start of an element. Used to get the ISO3166-2 name of each region.
-        function start($parser,$element_name,$element_attrs)
-          {
+        function start($parser,$element_name,$element_attrs) {
           global $mapArray;
           global $currentRegionCode;
-          switch($element_name)
-            {
+          switch($element_name) {
             case "GML:POLYGON":
                 $mapArray[$element_attrs["GML:ID"]] = 0;
                 $currentRegionCode = $element_attrs["GML:ID"];
             break;
-            }
           }
+        }
+
+
 
         //Function to use when finding character data. Converts the lat/lon data from GML to SVG.
-        function char($parser,$data)
-          {
+        function char($parser,$data) {
               global $mapArray;
               global $currentRegionCode;
               if (strlen($data) > 100){
@@ -34,9 +33,11 @@
                    * the L and the 20 25 means "draw a line from current point to this point, (10 15) to (20 25)" and
                    * the Z means close off the path.
                    */
+                  $data = latLonToXY($data);
                   $data = "M".trim($data)."Z";
                   $whiteSpaceCt = 0;
                   $strpos = 0;
+                  $latLonToXY = "";
                   while($strpos < strlen($data)){
                       if (substr($data, $strpos, 1) == " " && $whiteSpaceCt == 1){
                           $data = substr_replace($data, "L", $strpos, 1);
@@ -48,13 +49,66 @@
                   }
                   $mapArray[$currentRegionCode] = $data;
               }
-          }
+        }
 
-          //Function to use at the end of an element
-        function stop($parser,$element_name)
-          {
+        // Converts the lat and lon coords to cartesian xy coords. GML data is of form (lat, lon).
+        // @return returns a string with the converted lat lon values.
+        function latLonToXY($data) {
+            
+            $latLonToXY = "";
+            $lat = 0;
+            $lon = 0;
+            // Because the pos data in the gml files aren't always the same length, need a buffer to read diff lenghts.
+            $strBuffer = "";
+            $strpos = 0;
+            $whiteSpaceCt = 0;
+            while($strpos < strlen($data)){
+                      if (substr($data, $strpos, 1) == " " && $whiteSpaceCt == 1){
+                          // Even num of spaces delimit lon values.
+                          $lon = $strBuffer;
+                          $strBuffer = "";
+
+                          // Conversion to xy coords, rounded to 6 decimal precision. The 1000 is a map scale factor.
+                          // For sure there are better ways to do this.
+                          $x = (180 + $lon) * (1000/360);
+                          $x = round($x, 6);
+                          $y = (90 - $lat) * (1000/180);
+                          $y = round($y, 6);
+                          
+                          // I don't know why this one value is the only one messed up out of 100s of others.
+                          // Resultant latitude value is 54163 because buffer abruptly stops taking values, acting as if it read a space.
+                          if ($y < 1) {
+                            echo "wth?! <br />";
+                            echo "Y: ".$y."<br />";
+                            echo "lat: ".$lat."<br />";
+                          }
+                          
+                          // Attempt to normalize coords so that only positive values result.
+                          if ($x < 0)
+                              $x = abs($x);
+                          if ($y < 0)
+                              $y = abs($y);
+                          $latLonToXY .= $x." ".$y." ";
+                          $whiteSpaceCt = 0;
+                      } else if (substr($data, $strpos, 1) == " "){
+                          // Odd num of spaces delimits lat values.
+                          $lat = $strBuffer;
+                          $whiteSpaceCt++;
+                          $strBuffer = "";
+                      } else if (is_numeric(substr($data, $strpos, 1)) == 1 || substr($data, $strpos, 1) == "."){
+                          $strBuffer .= substr($data, $strpos, 1);
+                      }
+                      //echo "STRBUFF: ".$strBuffer."<br />";
+                      $strpos++;
+                  }
+             //echo "STR: ".$latLonToXY."<br />";
+             return $latLonToXY;
+        }
+
+        //Function to use at the end of an element
+        function stop($parser,$element_name) {
               //
-          }
+        }
 
         //Specify element handler
         xml_set_element_handler($parser,"start","stop");
@@ -66,13 +120,12 @@
         $mapFile = fopen($INPUT_FILE, 'r');
 
         //Read data
-        while ($data=fread($mapFile,131072))
-          {
+        while ($data=fread($mapFile,131072)) {
           xml_parse($parser,$data,feof($mapFile)) or
           die (sprintf("XML Error: %s at line %d",
           xml_error_string(xml_get_error_code($parser)),
           xml_get_current_line_number($parser)));
-          }
+        }
 
         //Free the XML parser
         xml_parser_free($parser);
